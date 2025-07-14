2dd06014776727e275f11ef0a848bdd0910c30f4
Henderson <hendersondike@gmail.com>
2025-07-14 04:11:12 +0100

    Enhance theming and typography in layout and dashboard
    - Added `@radix-ui/react-dropdown-menu` dependency in `package.json` and `package-lock.json` for improved UI components.
    - Updated global styles to change the dark background color and introduced a new font style for better typography.
    - Integrated `ThemeProvider` in the layout for enhanced theming capabilities.
    - Added a `ThemeToggle` component in the dashboard for user-friendly theme switching.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { Logout } from "@/components/logout";
+import { ThemeToggle } from "@/components/theme-toggle";
  
 export default async function DashboardPage() {
     const session = await auth.api.getSession({
@@ -16,9 +17,15 @@ export default async function DashboardPage() {
         <div className="flex flex-col h-screen">
             <div className="flex justify-end p-4">
                 <Logout />
+                <div className="ml-2 flex items-center">
+                    <ThemeToggle />
+                </div>
             </div>
+            
+               
+            
             <div className="flex justify-center items-center h-full">
-                <h1 className="text-2xl font-bold">Welcome {session.user.name}</h1>
+                <h1 className="text-md font-archivo-narrow">Welcome {session.user.name}</h1>
             </div>
             
         </div>
