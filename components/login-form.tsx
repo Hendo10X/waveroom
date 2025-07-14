a719d9bd623758ffcb292f86e8499f4dc91e5b56
Henderson <hendersondike@gmail.com>
2025-07-14 02:11:35 +0100

    Update dependencies and enhance login form functionality
    - Added `@hookform/resolvers` and updated `react-hook-form` and `zod` versions in `package.json` and `package-lock.json`.
    - Introduced a new font style for the login form and updated the layout in `layout.tsx`.
    - Refactored the login form to utilize `react-hook-form` with Zod validation for improved form handling and validation.
    - Enhanced the login form UI with Radix UI components for better structure and accessibility.

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
@@ -8,56 +7,102 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { signUp } from "@/server/users"
+import { signIn, signUp } from "@/server/users"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+
+const formSchema = z.object({
+  email: z.string(),
+  password: z.string().min(8),
+})
 
 export function LoginForm({
+  
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    signIn(values.email, values.password)
+    console.log(values)
+  }
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
+      <Card className="shadow-none">
         <CardHeader>
-          <CardTitle className="font-martian-mono text-xl tracking-tighter font-bold">
+          <CardTitle className="text-2xl tracking-tight font-bold text-center font-instrument-serif text-primary">
            Waveroom
           </CardTitle>
-          <CardDescription className="font-inter text-sm">
+          <CardDescription className="font-inter text-sm text-center text-muted-foreground">
             Enter your email below to login 
           </CardDescription>
         </CardHeader> 
         <CardContent>
-          <form className="font-inter">
+          <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
+                <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="you@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
               </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
+              <div className="grid gap-3"> 
+                <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input id="password" type="password" required {...field} placeholder="**********" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <a href="#" className="ml-auto text-sm text-muted-foreground hover:text-primary hover:underline">Forgot password?</a>
+              
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="button" className="w-full" onClick={() => signUp("test", "test@test.com", "password")}>
+                <Button type="submit" className="w-full bg-primary hover:bg-primary/90">
                   Login 
                 </Button>
                 <Button variant="outline" className="w-full">
                   Login with Google
-                </Button>
+                  </Button>
               </div>
             </div>
             <div className="mt-4 text-center text-sm">
@@ -67,6 +112,7 @@ export function LoginForm({
               </a>
             </div>
           </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
